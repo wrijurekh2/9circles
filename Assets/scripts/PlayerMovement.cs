6eaba6f499d34777d5c777737c687ee4bb526353
wrijurekh2 <wrijurekhmukherjee@gmail.com>
2026-06-07 20:59:55 +0100

    added cooldowns and limits and dashes that refresh

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Diagnostics;
+using System.Dynamic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,9 +13,11 @@ public class PlayerMovement : MonoBehaviour
     private Animator animator;
     private Vector2 movement;
     private bool Grounded;
-
-    private bool isDashing = false;
-
+    private int currDashCharges = 3;
+    public int maxDashCharges = 3; 
+    public float dashCooldown = 5f;
+    public float timer = 0f;
+    public bool isDashing = false;
     public Transform groundCheck;
     public float groundCheckRadius = 0.1f;
     public LayerMask groundLayer;
@@ -29,13 +32,13 @@ public class PlayerMovement : MonoBehaviour
     }
 
     IEnumerator Dash()
-    {
+    { 
         rb.linearVelocity = new Vector2(dashForce * movement.x, rb.linearVelocity.y);
         isDashing = true;
         yield return new WaitForSeconds(0.2f);
         rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
+        currDashCharges -= 1;
         isDashing = false;
-        
     }
 
     void Update()
@@ -49,16 +52,30 @@ public class PlayerMovement : MonoBehaviour
             groundLayer
         );
 
+        // keep track of timer
+        if (currDashCharges < maxDashCharges)
+        {
+            UnityEngine.Debug.Log("number of dash charges:" + currDashCharges);
+            timer += Time.deltaTime;
+            if (timer >= dashCooldown)
+            {
+
+                currDashCharges += 1;
+                timer = 0f;
+            }
+        }
+
         // Jump
         if (playerInput.Player.Jump.WasPressedThisFrame() && Grounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
 
-        if (playerInput.Player.Dash.WasPressedThisFrame() && !isDashing)
-        {
-            UnityEngine.Debug.Log("dashing");
+        if (playerInput.Player.Dash.WasPressedThisFrame() && !isDashing 
+                && currDashCharges > 0)
+        {   
             StartCoroutine(Dash());
+            
         }
 
         if(playerInput.Player.Attack.WasPressedThisFrame())
