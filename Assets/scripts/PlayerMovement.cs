93ce2f975f9ead968d76f775fe5b9f561a412384
wrijurekh2 <wrijurekhmukherjee@gmail.com>
2026-06-07 17:59:33 +0100

    updated input system to latest unity version and added a dash mechanic

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,27 +1,46 @@
+using System.Collections;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
+    private InputSystem_Actions playerInput;
     private Rigidbody2D rb;
     private Animator animator;
     private Vector2 movement;
     private bool Grounded;
 
+    private bool isDashing = false;
+
     public Transform groundCheck;
     public float groundCheckRadius = 0.1f;
     public LayerMask groundLayer;
+    public float dashForce = 15f;
 
     void Start()
     {
+        playerInput = new InputSystem_Actions();
+        playerInput.Enable();
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
+    IEnumerator Dash()
+    {
+        rb.linearVelocity = new Vector2(dashForce * movement.x, rb.linearVelocity.y);
+        isDashing = true;
+        yield return new WaitForSeconds(0.2f);
+        rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
+        isDashing = false;
+        
+    }
+
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.x = playerInput.Player.Move.ReadValue<Vector2>().x;
 
         // Check if grounded
         Grounded = Physics2D.OverlapCircle(
@@ -31,13 +50,18 @@ public class PlayerMovement : MonoBehaviour
         );
 
         // Jump
-        if (Input.GetButtonDown("Jump") && Grounded)
+        if (playerInput.Player.Jump.WasPressedThisFrame() && Grounded)
         {
-            Debug.Log("Jumping");
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
 
-        if(Input.GetButtonDown("Fire1"))
+        if (playerInput.Player.Dash.WasPressedThisFrame() && !isDashing)
+        {
+            UnityEngine.Debug.Log("dashing");
+            StartCoroutine(Dash());
+        }
+
+        if(playerInput.Player.Attack.WasPressedThisFrame())
         {
             animator.SetTrigger("Attack1");
         }
@@ -63,7 +87,9 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
-        
+        if (!isDashing)
+        {
+            rb.linearVelocity = new Vector2(movement.x * moveSpeed, rb.linearVelocity.y);
+        }
     }
 }
