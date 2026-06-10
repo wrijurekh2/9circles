67f5050b38dcec47db3ff379f1dee4332608925c
wrijurekh2 <wrijurekhmukherjee@gmail.com>
2026-06-10 12:15:15 +0100

    added healthbar and heartbeat and changed main character also fized some ui issues

--- Assets/scripts/HealthUI.cs
+++ Assets/scripts/HealthUI.cs
@@ -5,15 +5,19 @@ public class HealthUI : MonoBehaviour
     public float currentHealth = 10f;
     public float maxHealth = 100f;
     private Image fillImage;
+    private Animator heartAnimator;
     void Start()
     {
 
         fillImage = GetComponentsInChildren<Image>()[1];
+        heartAnimator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-        fillImage.fillAmount = currentHealth / maxHealth;
+        float targetFill = currentHealth / maxHealth;
+        fillImage.fillAmount = Mathf.Lerp(fillImage.fillAmount, targetFill, 5f * Time.deltaTime);
+        heartAnimator.speed = 0.3f + 2.5f * (1f - (currentHealth / maxHealth));
     }
 }
