a14d43178429da9c301742cbac14af3a33871e02
wrijurekh2 <wrijurekhmukherjee@gmail.com>
2026-06-20 12:39:39 +0100

    added post grapple jump

--- Assets/scripts/Grapple.cs
+++ Assets/scripts/Grapple.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
@@ -8,12 +6,17 @@ public class GrapplingHook : MonoBehaviour
     [SerializeField] private LayerMask grappleLayer;
     [SerializeField] private LineRenderer rope;
     [SerializeField] private float pullForce = 20f;
-    [SerializeField] private float arrivalThreshold = 1f;
+    [SerializeField] private float arrivalThreshold = 3f;
     private Rigidbody2D rb;
     private InputSystem_Actions playerInput;
     public bool isGrappling = false;
-
     private Vector3 grapplePoint;
+    private bool isThrowing = false; 
+    private Vector3 ropeEnd;
+    private float throwSpeed = 20f; 
+    private bool missed = false;
+    private PlayerMovement playerMovement;
+    public bool recentlyGrappled = false;
 
     void Start()
     {
@@ -21,36 +24,90 @@ public class GrapplingHook : MonoBehaviour
         playerInput = new InputSystem_Actions();
         playerInput.Enable();
         rb = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
 
     void Update()
     {
-        if(playerInput.Player.Grapple.WasPressedThisFrame())
+        if(playerInput.Player.Grapple.WasPressedThisFrame() && !isThrowing && !isGrappling)
         {
+            
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0;
+            
+            Vector2 aimDirection = (mouseWorldPos - transform.position).normalized;
+            
             RaycastHit2D hit = Physics2D.Raycast(
-            Camera.main.ScreenToWorldPoint(Input.mousePosition), 
-            Vector2.zero, 
-            Mathf.Infinity,
+            transform.position, 
+            aimDirection, 
+            grappleLength,
             grappleLayer);
+
+            //Debug.Log("Hit: " + hit.collider + " Distance: " + hit.distance);
+            //Debug.DrawRay(transform.position, aimDirection * grappleLength, Color.red, 2f);
 
             if(hit.collider !=null)
             {
                 grapplePoint = hit.point;
                 grapplePoint.z =0;
-                isGrappling = true;
-                rope.SetPosition(0, grapplePoint);
+                ropeEnd = transform.position;
+                isThrowing = true;
+                rope.SetPosition(0, transform.position);
+                rope.SetPosition(1, transform.position);
+                rope.enabled = true;
+            }
+            else
+            {
+                grapplePoint = (Vector3)(Vector2)transform.position + (Vector3)aimDirection * grappleLength;
+                grapplePoint.z = 0;
+                ropeEnd = transform.position;
+                isThrowing = true;
+                missed = true; 
+                rope.SetPosition(0, transform.position);
+                rope.SetPosition(1, transform.position);
                 rope.enabled = true;
             }
         }
 
-        if(playerInput.Player.Grapple.WasReleasedThisFrame())
+        if(isThrowing)
         {
-           Detach();
+            ropeEnd = Vector3.MoveTowards(ropeEnd, grapplePoint, throwSpeed * Time.deltaTime);
+            rope.SetPosition(0, ropeEnd);
+
+            if(Vector3.Distance(ropeEnd, grapplePoint) < 0.1f)
+            {
+                if(missed)
+                {
+                    missed = false;
+                    grapplePoint = transform.position;
+                }
+
+                else
+                {
+                    isThrowing = false;
+                    isGrappling = true;
+                }
+            }
+        }
+        if(isGrappling)
+        {
+            rope.SetPosition(0, grapplePoint);
         }
 
         if(rope.enabled)
         {
             rope.SetPosition(1, transform.position);
+        }
+
+        /*if(playerInput.Player.Grapple.WasReleasedThisFrame())
+        {
+           Detach();
+        }*/
+
+
+        if(missed && Vector3.Distance(ropeEnd, transform.position) < 0.1f)
+        {
+            Detach();
         }
     }
 
@@ -76,7 +133,9 @@ public class GrapplingHook : MonoBehaviour
     void Detach()
     {
         isGrappling = false;
+        isThrowing = false;
         rope.enabled = false;
         rb.gravityScale = 2;
+        recentlyGrappled = true;
     }
 }
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -4,37 +4,52 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Variables
+    [Header("Movement")]
     public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    private InputSystem_Actions playerInput;
-    private Rigidbody2D rb;
-    private Animator animator;
-    private Vector2 moveInput;
-    private bool Grounded;
-    public int currDashCharges = 3;
-    public int maxDashCharges = 3; 
-    public float dashCooldown = 5f;
-    public float timer = 0f;
-    public bool isDashing = false;
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.1f;
-    public LayerMask groundLayer;
-    public float dashForce = 10f;
-    public Transform wallCheck;
-    private float wallCheckRadius = 0.1f;
-    private bool isTouchingWall;
-    private float lastDirection = 1f;
     float acceleration = 8f;
     float deceleration = 8f;
     float velPower = 0.9f;
     float frictionAmount = 0.02f;
+    public float lastDirection = 1f;
+    private Vector2 moveInput;
+
+    [Header("Jump")]
+    public float jumpForce = 10f;
     float lastGroundedTime;
     float lastJumpTime;
     bool isJumping;
+
+    [Header("Dash")]
+    public float dashForce = 10f;
+    public int currDashCharges = 3;
+    public int maxDashCharges = 3;
+    public float dashCooldown = 5f;
+    public float timer = 0f;
+    public bool isDashing = false;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+    public LayerMask groundLayer;
+    public bool Grounded;
+
+    [Header("Wall Check")]
+    public Transform wallCheck;
+    private float wallCheckRadius = 0.1f;
+    private bool isTouchingWall;
+
+    [Header("Grapple")]
+    float postGrappleWindow = 0f;
+
+    // Components
+    private Rigidbody2D rb;
+    private Animator animator;
+    private InputSystem_Actions playerInput;
     private GrapplingHook grapplingHook;
+    #endregion
 
-
-
+    #region Unity Callbacks
     void Start()
     {
         playerInput = new InputSystem_Actions();
@@ -44,89 +59,29 @@ public class PlayerMovement : MonoBehaviour
         grapplingHook = GetComponent<GrapplingHook>();
     }
 
-    IEnumerator Dash()
-    { 
-        isDashing = true;
-        animator.SetBool("IsDashing", isDashing);
-        currDashCharges -= 1;
-        Debug.Log("Charges after decrease: " + currDashCharges);
-        Physics2D.IgnoreLayerCollision(
-            LayerMask.NameToLayer("Default"),
-            LayerMask.NameToLayer("Enemy"), 
-            true
-        );
-        
-
-        if(!Grounded)
-        {
-            rb.gravityScale = 0;
-            rb.linearVelocity = new Vector2(dashForce * lastDirection, 0);
-        }
-        else if(Grounded)
-        {
-            rb.linearVelocity = new Vector2(dashForce * lastDirection, rb.linearVelocity.y);
-        }
-
-        yield return new WaitForSeconds(0.5f);
-
-        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
-        rb.gravityScale = 2;
-        isDashing = false;
-        animator.SetBool("IsDashing", isDashing);
-        Physics2D.IgnoreLayerCollision(
-            LayerMask.NameToLayer("Default"),
-            LayerMask.NameToLayer("Enemy"), 
-            false
-        );
-    }
-
-    void Jump()
-    {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        isJumping = true;
-        lastGroundedTime = 0;
-        lastJumpTime = 0;
-        animator.SetTrigger("Jump"); 
-
-    }
-
-    public void OnJump()
-    {
-        lastJumpTime = 0.15f;
-    }
-    
-
     void Update()
     {
         moveInput.x = playerInput.Player.Move.ReadValue<Vector2>().x;
         if (moveInput.x != 0) lastDirection = moveInput.x;
+
         transform.position = new Vector3(
             Mathf.Clamp(transform.position.x, -49, 49),
             transform.position.y,
             transform.position.z
         );
+    
 
-        // Check if grounded
-        Grounded = Physics2D.OverlapCircle(
-            groundCheck.position,
-            groundCheckRadius,
-            groundLayer
-        );
+        #region Grounded and Wall Check
+        Grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isTouchingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, groundLayer);
+        #endregion
 
-        isTouchingWall = Physics2D.OverlapCircle(
-            wallCheck.position,
-            wallCheckRadius,
-            groundLayer
-        );
-
-
-        #region timer
+        #region Timers
         lastGroundedTime -= Time.deltaTime;
         lastJumpTime -= Time.deltaTime;
         #endregion
-        
 
-        // keep track of timer
+        #region Dash Charge Timer
         if (currDashCharges < maxDashCharges)
         {
             timer += Time.deltaTime;
@@ -136,38 +91,54 @@ public class PlayerMovement : MonoBehaviour
                 timer = 0f;
             }
         }
+        #endregion
 
-        // Jump
-        if (playerInput.Player.Jump.WasPressedThisFrame() && Grounded)
+        #region Jump
+
+        if(playerInput.Player.Jump.WasPressedThisFrame() && postGrappleWindow > 0)
         {
-            Jump();
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+            rb.AddForce(Vector2.up * jumpForce * 1.5f, ForceMode2D.Impulse);
         }
 
-        if(lastGroundedTime > 0 && lastJumpTime > 0 && !isJumping)
-        {
+        else if (playerInput.Player.Jump.WasPressedThisFrame() && Grounded)
             Jump();
-        }
 
-        if(Grounded)
+        else if (lastGroundedTime > 0 && lastJumpTime > 0 && !isJumping)
+            Jump();
+
+        if (Grounded)
         {
             lastGroundedTime = 0.15f;
             isJumping = false;
         }
+        #endregion
 
-        if (playerInput.Player.Dash.WasPressedThisFrame() && !isDashing 
-                && currDashCharges > 0)
-        {   
+        #region Dash
+        if (playerInput.Player.Dash.WasPressedThisFrame() && !isDashing && currDashCharges > 0)
+        {
             StartCoroutine(Dash());
             animator.SetTrigger("Dash");
         }
+        #endregion
 
-        if (playerInput.Player.Attack.WasPressedThisFrame())
+        #region grapple
+        if(grapplingHook.recentlyGrappled)
         {
-            CombatManager.instance.inputRecieved = true;
+            postGrappleWindow = 0.3f;
+            grapplingHook.recentlyGrappled = false;
         }
 
-        // Animations
+        postGrappleWindow -= Time.deltaTime;
 
+        #endregion
+
+        #region Attack
+        if (playerInput.Player.Attack.WasPressedThisFrame())
+            CombatManager.instance.inputRecieved = true;
+        #endregion
+
+        #region Animations
         if (moveInput.x != 0)
         {
             animator.SetBool("IsRunning", true);
@@ -182,7 +153,7 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             animator.SetBool("IsRunning", false);
-            if(!isDashing)
+            if (!isDashing)
             {
                 if (lastDirection > 0)
                     transform.localScale = new Vector3(-1, 1, 1);
@@ -190,30 +161,26 @@ public class PlayerMovement : MonoBehaviour
                     transform.localScale = new Vector3(1, 1, 1);
             }
         }
-        
-        
+
         animator.SetBool("IsFalling", rb.linearVelocity.y < -0.1f);
         animator.SetBool("IsGrounded", Grounded);
+        #endregion
     }
 
     void FixedUpdate()
     {
-        
-
-        #region run
+        #region Run
         if (!isDashing && !grapplingHook.isGrappling)
         {
-           float targetSpeed = moveInput.x * moveSpeed;
-           float speedDif = targetSpeed - rb.linearVelocity.x;
-           float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
-           float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
-           rb.AddForce(movement * Vector2.right);
-            
+            float targetSpeed = moveInput.x * moveSpeed;
+            float speedDif = targetSpeed - rb.linearVelocity.x;
+            float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+            float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+            rb.AddForce(movement * Vector2.right);
         }
         #endregion
-        
-        #region friction 
 
+        #region Friction
         if (Grounded && Mathf.Abs(moveInput.x) < 0.01f && !grapplingHook.isGrappling)
         {
             float amount = Mathf.Min(Mathf.Abs(rb.linearVelocity.x), Mathf.Abs(frictionAmount));
@@ -222,4 +189,55 @@ public class PlayerMovement : MonoBehaviour
         }
         #endregion
     }
+    #endregion
+
+    #region Movement Methods
+    void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        isJumping = true;
+        lastGroundedTime = 0;
+        lastJumpTime = 0;
+        animator.SetTrigger("Jump");
+    }
+
+    public void OnJump()
+    {
+        lastJumpTime = 0.15f;
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        animator.SetBool("IsDashing", isDashing);
+        currDashCharges -= 1;
+        Physics2D.IgnoreLayerCollision(
+            LayerMask.NameToLayer("Default"),
+            LayerMask.NameToLayer("Enemy"),
+            true
+        );
+
+        if (!Grounded)
+        {
+            rb.gravityScale = 0;
+            rb.linearVelocity = new Vector2(dashForce * lastDirection, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(dashForce * lastDirection, rb.linearVelocity.y);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+        rb.gravityScale = 2;
+        isDashing = false;
+        animator.SetBool("IsDashing", isDashing);
+        Physics2D.IgnoreLayerCollision(
+            LayerMask.NameToLayer("Default"),
+            LayerMask.NameToLayer("Enemy"),
+            false
+        );
+    }
+    #endregion
 }
